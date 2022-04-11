a56cb50e3647414bc12156686eb30a248457582e
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-04-11 19:16:27 -0400

    views

--- nyc/views.py
+++ nyc/views.py
@@ -17,9 +17,3 @@ class BoroughView(View):
             context={'borough': borough, 'activities': boroughs[borough].keys()},
         )
 
-class ActivityView(View):
-    pass
-
-
-class VenueView(View):
-   pass
